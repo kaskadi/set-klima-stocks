30e322621ed84010bcab3505fbd33933c993ca68
Alexis Lemaire <a.lemaire@klimapartner.de>
2020-09-09 16:07:40 +0200

    Temporarily commented out DB operation

--- set-klima-stocks.js
+++ set-klima-stocks.js
@@ -1,13 +1,15 @@
 module.exports.handler = async (event) => {
   const createBulkBody = require('./helpers/create-bulk-body.js')
   const setStocks = require('./helpers/set-es-stocks.js')
-  return await setStocks(createBulkBody(JSON.parse(event.Records[0].body)))
-  .then(res => {
-    console.log(JSON.stringify(res, null, 2))
-    return res
-  })
-  .catch(err => {
-    console.log(JSON.stringify(err, null, 2))
-    return err
-  })
+  console.log(JSON.stringify(event, null, 2))
+  return true
+  // return await setStocks(createBulkBody(JSON.parse(event.Records[0].body)))
+  // .then(res => {
+  //   console.log(JSON.stringify(res, null, 2))
+  //   return res
+  // })
+  // .catch(err => {
+  //   console.log(JSON.stringify(err, null, 2))
+  //   return err
+  // })
 }
